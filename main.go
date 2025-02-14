ab178f9f530fe44cab6b2521d5023fbea3016705
satyamkodale <satyamkodale@gmail.com>
2025-02-14 07:27:07 +0530

    added delete api

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -99,7 +100,24 @@ func getAllTodo(res http.ResponseWriter, req *http.Request) {
 }
 
 func deleteTodo(res http.ResponseWriter, req *http.Request) {
-
+	id := strings.TrimSpace(chi.URLParam(req, "id"))
+	// if id not found 
+	if !bson.IsObjectIdHex(id){
+				rnd.JSON(res,http.StatusBadRequest ,renderer.M{ "Message" : "Id not found" })
+				return
+	}
+	if err:=db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err!=nil{
+				rnd.JSON(w,http.StatusProcessing, renderer.M
+					{
+						"message": "Failed to delete todo",
+			      "error":   err,
+					})
+					return
+	}
+	rnd.JSON(w, http.StatusOK, renderer.M{
+				"message": "Todo deleted successfully",
+	})
+	
 }
 
 func createTodo(res http.ResponseWriter, req *http.Request) {
