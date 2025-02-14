4803813960d15f7ba223ef4fe96c672a16f832f6
satyamkodale <satyamkodale@gmail.com>
2025-02-14 06:28:55 +0530

    added router and handler for home(default)

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/thedevsaddam/renderer"
 
 	mgo "gopkg.in/mgo.v2"
 )
 
+var rnd *renderer.Render
+
+// Render is a Go package that helps simplify the process of writing JSON, HTML, or other responses in web applications.
 var db *mgo.Database
 
 const (
@@ -29,6 +39,25 @@ func checkErr(err error) {
 	}
 }
 
+func homeHandler(res http.ResponseWriter, req *http.Request) {
+	err := rnd.Template(res, http.StatusOK, []string{"static/home.tpl"}, nil)
+	checkErr(err)
+}
+
 func main() {
+	//creating channel
+	server := make(chan os.Signal)
+	signal.Notify(server, os.Interrupt)
 	fmt.Println("Setup done")
+
+	//creating router
+	// refers to a component that manages how incoming HTTP requests are directed to specific handlers
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", homeHandler)
+
+	//stop the server gresfully
+	<-server
+	log.Println("Shutting down server...")
+
 }
